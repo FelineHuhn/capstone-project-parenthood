4b8fdf3ff73f65404bbedd2f617aaa80a908af4c
FelineHuhn <112863528+FelineHuhn@users.noreply.github.com>
2022-11-16 16:28:06 +0100

    update margin in SpotCard component
    Co-authored-by: Jessica Lörs <54777853+JessicaLoers@users.noreply.github.com>

--- components/SpotCard.js
+++ components/SpotCard.js
@@ -18,7 +18,7 @@ const Card = styled.li`
   background-color: white;
   border: 1px solid black;
   border-radius: 10px;
-  margin: 20px 20px 0px -20px;
+  margin: 20px;
   display: flex;
   justify-content: space-between;
 `;
