fc8d8361f709d623a815577e79b30e5f8d4ec4fe
Feline Huhn <feline.huhn@gmx.de>
2022-11-21 15:31:17 +0100

    fix spot details

--- helpers/mockSpots.js
+++ helpers/mockSpots.js
@@ -4,10 +4,10 @@ export const spotsList = [
     category: "Spielplatz",
     name: "Baschu",
     addresse: "https://goo.gl/maps/DTxaT3ThkfjmQ2T17",
-    weather: ["bei gutem Wetter", "bei mäßigem Wetter"],
-    age: ["Kinder 1 - 3 Jahre", "Kinder 3 - 6 Jahre"],
+    weathersuggestion: ["bei gutem Wetter", "bei mäßigem Wetter"],
+    agesuggestion: ["Kinder 1 - 3 Jahre", "Kinder 3 - 6 Jahre"],
     tags: ["seilbahn", "rutsche", "klettern"],
-    infos:
+    information:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
@@ -15,10 +15,10 @@ export const spotsList = [
     category: "Spielplatz",
     name: "Brockmannsweg",
     addresse: "https://goo.gl/maps/58sX5wzQARdeVpzY9",
-    weather: ["bei gutem Wetter", "bei mäßigem Wetter"],
-    age: ["Kinder 1 - 3 Jahre", "Kinder 3 - 6 Jahre"],
+    weathersuggestion: ["bei gutem Wetter", "bei mäßigem Wetter"],
+    agesuggestion: ["Kinder 1 - 3 Jahre", "Kinder 3 - 6 Jahre"],
     tags: ["rutsche", "schaukel"],
-    infos:
+    information:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
@@ -26,10 +26,14 @@ export const spotsList = [
     category: "Indoor Spielplatz",
     name: "rabatzz!",
     addresse: "https://goo.gl/maps/qrpNmEfyTZ7D1w2h9",
-    weather: ["bei schlechtem Wetter"],
-    age: ["Kinder 3 - 6 Jahre", "Kinder 6 - 9 Jahre", "Kinder 9 - 12 Jahre"],
+    weathersuggestion: ["bei schlechtem Wetter"],
+    agesuggestion: [
+      "Kinder 3 - 6 Jahre",
+      "Kinder 6 - 9 Jahre",
+      "Kinder 9 - 12 Jahre",
+    ],
     tags: ["indoor", "trampolin", "rutschen"],
-    infos:
+    information:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
@@ -37,10 +41,10 @@ export const spotsList = [
     category: "Sportplatz",
     name: "ETV Fußball",
     addresse: "https://goo.gl/maps/2pMQG2DajHSwqMtw6",
-    weather: ["bei gutem Wetter", "bei mäßigem Wetter"],
-    age: ["Kinder 6 - 9 Jahre", "Kinder 9 - 12 Jahre", "Jugendliche"],
+    weathersuggestion: ["bei gutem Wetter", "bei mäßigem Wetter"],
+    agesuggestion: ["Kinder 6 - 9 Jahre", "Kinder 9 - 12 Jahre", "Jugendliche"],
     tags: ["fußball", "kicken", "verein"],
-    infos:
+    information:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
@@ -48,10 +52,14 @@ export const spotsList = [
     category: "Wald",
     name: "Forst Klövensteen",
     addresse: "https://goo.gl/maps/o6RWhHDkQ14K1zy16",
-    weather: ["bei gutem Wetter", "bei mäßigem Wetter"],
-    age: ["Kinder 3 - 6 Jahre", "Kinder 6 - 9 Jahre", "Kinder 9 - 12 Jahre"],
+    weathersuggestion: ["bei gutem Wetter", "bei mäßigem Wetter"],
+    agesuggestion: [
+      "Kinder 3 - 6 Jahre",
+      "Kinder 6 - 9 Jahre",
+      "Kinder 9 - 12 Jahre",
+    ],
     tags: ["natur"],
-    infos:
+    information:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
 ];
