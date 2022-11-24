8d85452c8af067d30835f58b07d7a25d2be1591c
Feline Huhn <feline.huhn@gmx.de>
2022-11-24 14:57:06 +0100

    add link to navigate to edit page from details section

--- components/SpotDetails.js
+++ components/SpotDetails.js
@@ -71,13 +71,7 @@ export default function SpotDetails({ spot, addSpot, deleteSpot, editSpot }) {
         </Background>
       )}
       <ButtonStyling>
-        <Button
-          type="button"
-          variant="edit"
-          onClick={() => {
-            setIsShownForm((prevState) => !prevState);
-          }}
-        >
+        <Link href={`/edit`}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24px"
@@ -88,7 +82,8 @@ export default function SpotDetails({ spot, addSpot, deleteSpot, editSpot }) {
             <path d="M0 0h24v24H0z" fill="none" />
             <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
           </svg>
-        </Button>
+        </Link>
+
         <Button
           type="button"
           variant="delete"
