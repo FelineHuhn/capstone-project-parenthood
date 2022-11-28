4cf624b4d2a6496d9f8fa3b3d02a9102d14bc471
Feline Huhn <feline.huhn@gmx.de>
2022-11-28 14:24:16 +0100

    create favorites page and add it to navigation

--- components/Footer.js
+++ components/Footer.js
@@ -28,6 +28,18 @@ export default function Footer() {
           <path d="M12 2C8.14 2 5 5.14 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.86-3.14-7-7-7zm4 8h-3v3h-2v-3H8V8h3V5h2v3h3v2z" />
         </svg>
       </Link>
+      <Link href={`/favorites`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="30px"
+          viewBox="0 0 24 24"
+          width="30px"
+          fill="white"
+        >
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+        </svg>
+      </Link>
     </StyledFooter>
   );
 }
---
+++ pages/favorites/index.js
@@ -0,0 +1,5 @@
+import Header from "../../components/Header";
+
+export default function FavoriteSpots() {
+  return <Header>Meine Lieblings-Spots</Header>;
+}
