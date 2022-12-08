09ab5d66e93b0980b5af6e54a1facb7aa0f84a8a
Feline Huhn <feline.huhn@gmx.de>
2022-12-08 16:41:46 +0100

    fix styling in landingpage

--- pages/index.js
+++ pages/index.js
@@ -86,8 +86,9 @@ const LinkToSpots = styled(Link)`
   @media (min-width: 376px) {
     font-size: 1.1rem;
     position: absolute;
-    bottom: 200px;
+    bottom: 510px;
     z-index: 20;
+    position: relative;
     box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
       rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
   }
